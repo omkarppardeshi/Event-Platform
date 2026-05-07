920f45b4c7ddfd25b89b77209d2b6cc1855526d4
Omkar Pardeshi <omkar.17.pardeshi@gmail.com>
2026-05-07 21:24:05 +0530

    BaseURL updated

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://event-platform-dtss.onrender.com/",
 });
 
 api.interceptors.request.use((config) => {
