1a62b730d524b21576f343e392e2769fd1cd86b1
Omkar Pardeshi <omkar.17.pardeshi@gmail.com>
2026-05-07 21:31:56 +0530

    extra / remove

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://event-platform-dtss.onrender.com/",
+  baseURL: "https://event-platform-dtss.onrender.com",
 });
 
 api.interceptors.request.use((config) => {
